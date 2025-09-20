c5969b65749681d898390c7400ffddba99dedd25
= <=>
2025-09-20 19:00:23 +0700

    19:00 20/9

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,5 +1,45 @@
 
 
+// import React, { useState } from "react";
+// import classNames from "classnames/bind";
+// import styles from "./List.module.scss";
+// import UserInfo from "./userInfo";
+// import ChatList from "./chatList";
+// import FriendList from "./friendList";
+
+// const cx = classNames.bind(styles);
+
+// function List({ onSelectFriend, selectedFriend, datax, onUpdateChat }) {
+//   const [isSearchExpanded, setIsSearchExpanded] = useState(false);
+
+//   console.log("List : ", datax);
+
+//   return (
+//     <div className={cx("list")}>
+//       <UserInfo
+//         isSearchExpanded={isSearchExpanded}
+//         setIsSearchExpanded={setIsSearchExpanded}
+//         onSelectFriend={onSelectFriend} // Truyền callback vào UserInfo (cho AddFriend)
+//       />
+//       {isSearchExpanded ? (
+//         <FriendList
+//           onSelectFriend={onSelectFriend} 
+//           selectedFriend={selectedFriend} 
+//         /> // Truyền callback và selectedFriend
+//       ) : (
+//         <ChatList
+//           onSelectFriend={onSelectFriend} 
+//           selectedFriend={selectedFriend} 
+//           chatData1={datax}
+//           onUpdateChat={onUpdateChat}
+//         /> // Truyền callback và selectedFriend
+//       )}
+//     </div>
+//   );
+// }
+
+// export default List;
+
 import React, { useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./List.module.scss";
@@ -9,29 +49,33 @@ import FriendList from "./friendList";
 
 const cx = classNames.bind(styles);
 
-function List({ onSelectFriend, selectedFriend, datax }) {
+function List({ onSelectFriend, selectedFriend, datax, onUpdateChat, setUpdateChatList }) {
   const [isSearchExpanded, setIsSearchExpanded] = useState(false);
-
-  console.log("List : ", datax);
+  
+  console.log("List props:", { datax, selectedFriend });
 
   return (
     <div className={cx("list")}>
       <UserInfo
         isSearchExpanded={isSearchExpanded}
         setIsSearchExpanded={setIsSearchExpanded}
-        onSelectFriend={onSelectFriend} // Truyền callback vào UserInfo (cho AddFriend)
+        onSelectFriend={onSelectFriend}
+        datax={datax}
       />
       {isSearchExpanded ? (
         <FriendList
-          onSelectFriend={onSelectFriend} 
-          selectedFriend={selectedFriend} 
-        /> // Truyền callback và selectedFriend
+          onSelectFriend={onSelectFriend}
+          selectedFriend={selectedFriend}
+          datax={datax}
+        />
       ) : (
         <ChatList
-          onSelectFriend={onSelectFriend} 
-          selectedFriend={selectedFriend} 
+          onSelectFriend={onSelectFriend}
+          selectedFriend={selectedFriend}
           chatData1={datax}
-        /> // Truyền callback và selectedFriend
+          onUpdateChat={onUpdateChat}
+          setUpdateChatList={setUpdateChatList} // Truyền setUpdateChatList xuống ChatList
+        />
       )}
     </div>
   );
